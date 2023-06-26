96e279b3c3e40593d188ae933567e4cf934190a4
Ch4rl <Ch4rl@LAPTOP-GV845E6E>
2023-06-26 11:36:47 -0600

    Creación del Proyecto DWP Grupo 4 Jurásico Kids

---
+++ src/main/java/com/proyectoG4/ProyectoG4Application.java
@@ -0,0 +1,13 @@
+package com.proyectoG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG4Application.class, args);
+	}
+
+}
